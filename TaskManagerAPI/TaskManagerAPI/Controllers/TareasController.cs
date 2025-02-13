0dde2afdb6fbf536f4421ca8d6feb3404ab9183e
Alfarog507 <alfarog507@gmail.com>
2025-02-13 16:55:27 -0600

    Mejoramos la estructura de TareasController

--- TaskManagerAPI/TaskManagerAPI/Controllers/TareasController.cs
+++ TaskManagerAPI/TaskManagerAPI/Controllers/TareasController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TaskManagerAPI.Models;
 
@@ -17,86 +12,119 @@ namespace TaskManagerAPI.Controllers
 
         public TareasController(TaskManagerDbContext context)
         {
-            _context = context;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
         // GET: api/Tareas
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Tarea>>> GetTareas()
         {
-            return await _context.Tareas.ToListAsync();
+            try
+            {
+                var tareas = await _context.Tareas.ToListAsync();
+                return Ok(tareas);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         // GET: api/Tareas/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Tarea>> GetTarea(int id)
         {
-            var tarea = await _context.Tareas.FindAsync(id);
-
-            if (tarea == null)
+            try
             {
-                return NotFound();
-            }
+                var tarea = await _context.Tareas.AsNoTracking().FirstOrDefaultAsync(t => t.IdTarea == id);
 
-            return tarea;
+                if (tarea == null)
+                {
+                    return NotFound();
+                }
+                return Ok(tarea);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         // PUT: api/Tareas/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTarea(int id, Tarea tarea)
+        public async Task<IActionResult> PutTarea(int id, [FromBody] Tarea tarea)
         {
             if (id != tarea.IdTarea)
             {
-                return BadRequest();
+                return BadRequest(new { mensaje = "El ID no coincide" });
             }
 
-            _context.Entry(tarea).State = EntityState.Modified;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
             try
             {
+                _context.Entry(tarea).State = EntityState.Modified;
                 await _context.SaveChangesAsync();
+                return Ok(new { mensaje = "Tarea actualizada" });
             }
             catch (DbUpdateConcurrencyException)
             {
                 if (!TareaExists(id))
                 {
-                    return NotFound();
+                    return NotFound(new { mensaje = "Tarea no encontrada" });
                 }
-                else
-                {
-                    throw;
-                }
+                return StatusCode(StatusCodes.Status500InternalServerError, new { mensaje = "Error al actualizar la tarea" });
             }
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         // POST: api/Tareas
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Tarea>> PostTarea(Tarea tarea)
+        public async Task<ActionResult<Tarea>> PostTarea([FromBody] Tarea tarea)
         {
-            _context.Tareas.Add(tarea);
-            await _context.SaveChangesAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-            return CreatedAtAction("GetTarea", new { id = tarea.IdTarea }, tarea);
+            try
+            {
+                _context.Tareas.Add(tarea);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetTarea", new { id = tarea.IdTarea }, tarea);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         // DELETE: api/Tareas/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteTarea(int id)
         {
-            var tarea = await _context.Tareas.FindAsync(id);
-            if (tarea == null)
+            try
             {
-                return NotFound();
+                var tarea = await _context.Tareas.FindAsync(id);
+                if (tarea == null)
+                {
+                    return NotFound(new { mensaje = "Tarea no encontrada" });
+                }
+                _context.Tareas.Remove(tarea);
+                await _context.SaveChangesAsync();
+                return Ok(new { mensaje = "Tarea eliminada" });
             }
-
-            _context.Tareas.Remove(tarea);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         private bool TareaExists(int id)
